ee332ede2f446154fee6919a4cef0dfcdf4875b5
Filipe Galvão <filipe.silva@sensedia.com>
2024-09-15 15:47:46 -0300

    [feat] Custom list Readme Template

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ import { documenter } from "./documenter.js";
 import { generateMainReadme } from "./generateMainReadme.js";
 import { closeBrowser } from "./browser.js";
 
-// await login();
+await login();
 
 const listInfo = await getChallengesLinksFromList(
   process.env.CHALLENGES_LIST_URL
 );
 
-// let count = 0;
-// for (const challengeLink of listInfo.challengeLinks) {
-//   console.log(`Documenting challenge ${++count}/${listInfo.challengeLinks.length} from ${challengeLink.url}`);
-//   await documenter(listInfo.listName, challengeLink.category, challengeLink.url);
-// }
+let count = 0;
+for (const challengeLink of listInfo.challengeLinks) {
+  console.log(`Documenting challenge ${++count}/${listInfo.challengeLinks.length} from ${challengeLink.url}`);
+  await documenter(listInfo.listName, challengeLink.category, challengeLink.url);
+}
 
 await closeBrowser();
 
